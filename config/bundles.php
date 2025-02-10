f94a94c6dad647f470c0928c90548c9497fd770b
Maxime <115540245+JAURAS-Maxime-2225046a@users.noreply.github.com>
2025-02-10 17:21:02 +0100

    fix: accès non autorisé aux pages après authentification

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Symfony\UX\Translator\UxTranslatorBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
